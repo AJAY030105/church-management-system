1b4fe434d8143c1ae42217d25590bbde5ef27bcb
Ajay <ajay03krishna@gmail.com>
2026-03-29 12:15:19 +0530

    updated frontend API url

--- backend/server.js
+++ backend/server.js
@@ -11,12 +11,14 @@ const quizRoutes = require('./routes/quizRoutes');
 connectDB();
 
 const app = express();
-
-
+const cors = require("cors");
 
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:5173',
-  credentials: true,
+  origin: [
+    "http://localhost:5173",
+    "https://church-management-system-delta.vercel.app"
+  ],
+  credentials: true
 }));
 
 app.use(express.json());
