250f28cd4db25b5a88424653c5456cab3b704c91
Ajay <ajay03krishna@gmail.com>
2026-03-29 11:51:02 +0530

    updated backend API url

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: '/api',
+  baseURL: "https://church-backend-k6mf.onrender.com/api",
   headers: { 'Content-Type': 'application/json' },
 });
 
